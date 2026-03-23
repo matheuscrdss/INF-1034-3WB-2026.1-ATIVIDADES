90bc3135dd5862a3753f0f026f25d2fdedf3c9d7
matheus cardoso <matheuscrdss12@gmail.com>
2026-03-23 12:36:02 -0300

    Atividade 2.2

---
+++ bandeiras.py
@@ -0,0 +1,247 @@
+from turtle import *
+import time
+
+t = Turtle()
+t.speed(0)
+#japao - facil
+
+# t.pencolor('black')
+# t.fillcolor('white')
+# t.begin_fill()
+
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(200)
+#     t.lt(90)
+
+# t.end_fill()
+
+# t.pu()
+# t.goto(150,60)
+# t.pd()
+
+
+# t.fillcolor('red')
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+
+# time.sleep(5)
+# t.clear()
+
+
+#italia - facil
+
+# t.fillcolor('black')
+# t.begin_fill()
+
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(200)
+#     t.lt(90)
+
+# t.fillcolor('green')
+# t.begin_fill()
+# for i in range(2):
+#     t.fd(100)
+#     t.lt(90)
+#     t.fd(100)
+# t.end_fill()
+
+
+# t.pu()
+# t.goto(0,0)
+# t.pd()
+# t.lt(180)
+# t.fd(200)
+
+# t.fillcolor('red')
+# t.begin_fill()
+
+# for i in range(2):
+#     t.fd(100)
+#     t.lt(90)
+#     t.fd(100)
+# t.lt(90)
+# t.fd(200)
+
+# t.end_fill()
+
+# time.sleep(5)
+# t.clear
+
+#india - dificil
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(200)
+#     t.lt(90)
+
+# t.fillcolor('green')
+# t.begin_fill()
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(70)
+#     t.lt(90)
+
+# t.end_fill()
+
+# t.pu()
+# t.goto(0,0)
+# t.pd()
+# t.lt(90)
+# t.fd(130)
+# t.right(90)
+
+# t.fillcolor('orange')
+# t.begin_fill()
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(70)
+#     t.lt(90)
+
+# t.end_fill()
+
+# t.pu()
+# t.goto(150,80)
+# t.pd()
+# t
+# t.circle(20)
+
+#emirados arabes - medio
+
+# t.pencolor('black')
+# t.fillcolor('black')
+# t.begin_fill()
+
+
+# for i in range(2):
+#     t.fd(300)
+#     t.lt(90)
+#     t.fd(200)
+#     t.lt(90)
+
+# t.fillcolor('red')
+# t.begin_fill()
+# for i in range(2):
+#     t.fd(100)
+#     t.lt(90)
+#     t.fd(100)
+# t.end_fill()
+
+# t.pu()
+# t.goto(0,0)
+# t.pd()
+# t.lt(180)
+# t.fd(100)
+
+# t.fillcolor('black')
+# t.begin_fill()
+
+# for i in range(2):
+#     t.fd(200)
+#     t.lt(90)
+#     t.fd(65)
+#     t.lt(90)
+
+# t.end_fill()
+
+# t.lt(90)
+# t.fd(200)
+# t.right(90)
+
+# t.fillcolor('green')
+# t.begin_fill()
+
+# for i in range(2):
+#     t.fd(200)
+#     t.right(90)
+#     t.fd(65)
+#     t.right(90)
+
+# t.end_fill()
+
+
+#senegal - medio
+
+t.fillcolor('black')
+t.begin_fill()
+
+
+for i in range(2):
+    t.fd(300)
+    t.lt(90)
+    t.fd(200)
+    t.lt(90)
+
+t.fillcolor('green')
+t.begin_fill()
+for i in range(2):
+    t.fd(100)
+    t.lt(90)
+    t.fd(100)
+t.end_fill()
+
+
+t.pu()
+t.goto(0,0)
+t.pd()
+t.lt(180)
+t.fd(200)
+
+t.fillcolor('red')
+t.begin_fill()
+
+for i in range(2):
+    t.fd(100)
+    t.lt(90)
+    t.fd(100)
+t.lt(90)
+t.fd(200)
+
+t.end_fill()
+
+t.pu()
+t.goto(0,0)
+t.pd()
+t.lt(90)
+t.fd(100)
+
+t.fillcolor('yellow')
+t.begin_fill()
+
+for i in range(2):
+    t.fd(100)
+    t.lt(90)
+    t.fd(200)
+    t.lt(90)
+
+t.end_fill()
+
+t.pu()
+t.goto(120,110)
+t.pd()
+
+t.fillcolor('green')
+t.begin_fill()
+
+for i in range(5):
+    t.fd(60)
+    t.right(144)
+
+t.end_fill()
+
+t.pu()
+t.goto(140,90)
+t.pd()
+t.circle(10)
+mainloop()
