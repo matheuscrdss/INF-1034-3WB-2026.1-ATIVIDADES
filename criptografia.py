2a2e5e8bbc27daf3e9b0e1cf713ff8e15ef874d5
matheus cardoso <matheuscrdss12@gmail.com>
2026-05-11 00:15:46 -0300

    Atividade 8

--- criptografia.py
+++ criptografia.py
@@ -1,7 +1,6 @@
-import string
-
-#email = input("Digite o email: ")
-senha = input("Digite uma senha: ")
+from pygame import *
+import subprocess # descobri pesquisando !
+import sys
 
 def valida_email(email):
     if "@puc.com" in email:
@@ -11,4 +10,135 @@ def valida_email(email):
 
 def tem_maiuscula(senha):
     for carac in senha:
+        if "A" <= carac <= "Z":
+            return True
+    return False
 
+def tem_minuscula(senha):
+    for carac in senha:
+        if "a" <= carac <= "z":
+            return True
+    return False
+
+def tem_numero(senha):
+    for carac in senha:
+        if carac.isdigit():
+            return True
+    return False
+
+def valida_senha(senha):
+    if len(senha) < 8:
+        return False
+    if not tem_maiuscula(senha):
+        return False
+    if not tem_minuscula(senha):
+        return False
+    if not tem_numero(senha):
+        return False
+    return True
+
+def criptografa(senha):
+    senha_cripto = ""
+
+    for carac in senha:
+        if carac.isalpha():
+            pos_alpha = ord(carac) - ord('a')
+            pos_alpha = (pos_alpha + 3) % 26
+            pos_ascii = pos_alpha + ord('a')
+            senha_cripto += chr(pos_ascii)
+    return senha_cripto
+
+
+init()
+screen = display.set_mode((1280, 720))
+running = True
+
+fonte = font.Font(None, 45)
+fonte_menor = font.Font(None, 32)
+
+email = ""
+senha = ""
+campo_atual = "email"
+
+mensagem_email = ""
+mensagem_senha = ""
+senha_criptografada = ""
+
+while running:
+    for evento in event.get():
+        if evento.type == QUIT:
+            running = False
+
+        if evento.type == KEYDOWN:
+            if evento.key == K_BACKSPACE:
+                if campo_atual == "email":
+                    email = email[:-1]
+                else:
+                    senha = senha[:-1]
+
+            elif evento.key == K_RETURN:
+                if campo_atual == "email":
+                    if valida_email(email):
+                        mensagem_email = "Parabéns, o seu e-mail é válido!"
+                        campo_atual = "senha"
+                    else:
+                        mensagem_email = "Email inválido!"
+
+                elif campo_atual == "senha":
+                    if valida_senha(senha):
+                        mensagem_senha = "Senha segura!"
+                        senha_criptografada = criptografa(senha)
+
+                        if senha_criptografada != "":
+                            texto = fonte_menor.render("Senha criptografada:", True, "black")
+                            screen.blit(texto, (100, 470))
+
+                            cripto = fonte.render(senha_criptografada, True, "blue")
+                            screen.blit(cripto, (100, 510))
+
+                        display.update()
+                        time.delay(1000)
+
+                        quit()
+
+                        subprocess.run([sys.executable, "Atividade 6/casita.py"])
+                        running = False
+
+                    else:
+                        mensagem_senha = "Senha fraca! Use 8 caracteres, maiúscula, minúscula e número."
+
+            else:
+                if campo_atual == "email":
+                    email += evento.unicode
+                else:
+                    senha += evento.unicode
+
+    screen.fill("white")
+
+    texto_email = fonte_menor.render("Digite seu email:", True, "black")
+    screen.blit(texto_email, (100, 140))
+
+    draw.rect(screen, "black", (100, 170, 600, 45), 2)
+    email_surface = fonte_menor.render(email, True, "black")
+    screen.blit(email_surface, (110, 182))
+
+    if mensagem_email != "":
+        cor = "green" if valida_email(email) else "red"
+        msg = fonte_menor.render(mensagem_email, True, cor)
+        screen.blit(msg, (100, 230))
+
+    texto_senha = fonte_menor.render("Digite sua senha:", True, "black")
+    screen.blit(texto_senha, (100, 300))
+
+    draw.rect(screen, "black", (100, 330, 600, 45), 2)
+    senha_surface = fonte_menor.render(senha, True, "black")
+    screen.blit(senha_surface, (110, 342))
+
+    if mensagem_senha != "":
+        cor = "green" if valida_senha(senha) else "red"
+        msg = fonte_menor.render(mensagem_senha, True, cor)
+        screen.blit(msg, (100, 390))
+
+    display.update()
+
+quit()
--- main.py
+++ main.py
@@ -1,85 +1,90 @@
-from turtle import *
+import pygame
 
-t = Turtle()
+## Definições de funções
+# def valida_email(email):
+#     sufixo = email[-8:]
+#     if sufixo == "@puc.com":
+#         return True
+#     else:
+#         return False
 
-color = textinput('Escolhe uma cor', 'Digite: ')
-t.speed(100)
+def valida_email(email):
+    return email[-8:] == "@puc.com"
 
-# CARTESIANO
-t.fillcolor(color)
-t.begin_fill()
+def possui_maiuscula(senha):
+    for carac in senha:
+        if 'A' <= carac <= 'Z':
+            return True
+    return False
 
-t.fd(500)
-t.pu()
-t.goto(0,0)
-t.pd()
-t.back(500)
+def possui_minuscula(senha):
+    for carac in senha:
+        if 'a' <= carac <= 'z':
+            return True
+    return False
 
-t.pu()
-t.goto(0,0)
-t.pd()
-t.lt(90)
-t.fd(500)
+def possui_numero(senha):
+    for carac in senha:
+        if carac.isnumeric():
+            return True
+    return False
 
-t.pu()
-t.goto(0,0)
-t.pd()
-t.back(500)
+def valida_senha(senha):
+    if len(senha) < 8:
+        return False
+    if not possui_maiuscula(senha):
+        return False
+    if not possui_minuscula(senha):
+        return False
+    if not possui_numero(senha):
+        return False
+    
+    return True
 
-t.goto(0,0)
-t.end_fill()
+def criptografa(senha):
+    senha_cripto = ""
+    for carac in senha:
+        if carac.isalpha():
+            pos_alpha = ord(carac) - ord('a')
+            pos_alpha = (pos_alpha + 3) % 26
+            pos_ascii = pos_alpha + ord('a')
+            senha_cripto += chr(pos_ascii)
+    return senha_cripto
 
-#octagono
-t.pu()
-t.goto(350,150)
-t.pd()
+## Configurar e utilizar o pygame
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = True
 
-t.fillcolor(color)
-t.begin_fill()
-for i in range(8):
-    t.fd(100)
-    t.lt(45)
-t.end_fill()
+input_string = ""
+valid_email = False
+pressed_enter = False
+fonte = pygame.font.Font(size=50)
+fonte_validation = pygame.font.Font(size=25)
 
-#losango
-t.pu()
-t.goto(-300,-200)
-t.pd()
-t.setheading(0)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                input_string = input_string[:-1]
+            elif event.key == pygame.K_RETURN:
+                pressed_enter = True
+                valid_email = valida_email(input_string)
+            else:
+                input_string += event.unicode
 
-t.fillcolor(color)
-t.begin_fill()
-for i in range(2):
-    t.fd(150)   
-    t.lt(60)
-    t.fd(150)   
-    t.lt(120)
-t.end_fill()
+    screen.fill("white")
+    pygame.draw.rect(screen, "black", (100, 100, 500, 50), 3)
+    input_text = fonte.render(input_string, True, "#000000")
+    screen.blit(input_text, (120, 105))
+    if pressed_enter:
+        if valid_email:
+            valid_text = fonte_validation.render("Parabéns, o seu e-mail é válido!", True, "green")
+            screen.blit(valid_text, (120, 180))
+        else:
+            valid_text = fonte_validation.render("O seu e-mail não é válido. Digite novamente!", True, "red")
+            screen.blit(valid_text, (120, 180))
 
-# triangulo
-t.pu()
-t.goto(150,-200)
-t.pd()
-t.setheading(0)
-
-t.fillcolor(color)
-t.begin_fill()
-for i in range(3):
-    t.fd(150)
-    t.lt(120)
-t.end_fill()
-
-#espiral
-t.pu()
-t.goto(-200,150)
-t.pd()
-t.setheading(0)
-
-t.fillcolor(color)
-t.begin_fill()
-for i in range(100):
-    t.fd(i * 2)
-    t.lt(90)
-t.end_fill()
-
-mainloop()
+    pygame.display.update()
